03a08c181d67efb518c2799fa83d336fc96dc943
User <User@DESKTOP-DVTU071>
2025-04-24 00:33:17 +0400

    Добавьте файлы проекта.

---
+++ Models/Predmet.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace tp5.Models
+{
+    public class Predmet
+    {
+        [DisplayName("ID курса")]
+        public int CourseId { get; set; }
+
+        [DisplayName("Название курса")]
+        public string CourseName { get; set; }
+
+        [DisplayName("Описание курса")]
+        public string Description { get; set; }
+
+        [DisplayName("Дата начала")]  
+        public DateTime StartDate { get; set; }
+
+        [DisplayName("Дата окончания")]
+        public string EndDate { get; set; }
+
+        [DisplayName("Преподаватель")]
+        public string Instructor { get; set; }
+
+        [DisplayName("Телефон преподавателя")]
+        [DataType(DataType.PhoneNumber)]
+        public string InstructorPhone { get; set; }
+    }
+}
